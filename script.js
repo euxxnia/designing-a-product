a9a58e7d43075044a986fedc1232dc120422efe5
euxxnia <syoomch12@gmail.com>
2024-06-14 14:26:13 +0900

    0614

---
+++ script.js
@@ -0,0 +1,41 @@
+const modal = document.getElementById('modal');
+const modalImage = document.getElementById('modal-image');
+const modalDescription = document.getElementById('modal-description');
+const additionalImages = document.getElementById('additional-images');
+
+const images = [
+    { src: 'backServer.png' },
+    { src: 'backLanguage.png' },
+    { src: 'backDeveloper.png' },
+    { src: 'backEngineering.png' },
+    { src: 'backAi.png' },
+    { src: 'backCooperation.png' },
+    { src: 'backProduct.png' },
+    { src: 'backWeb.png' },
+    { src: 'backUser.png' }
+];
+
+let currentImageIndex = 0;
+
+function openModal(index) {
+    currentImageIndex = index;
+    const modal = document.getElementById('modal');
+    const modalImg = document.getElementById('modalImg');
+    const modalText = document.getElementById('modalText');
+    modal.style.display = "flex";
+    modalImg.src = images[currentImageIndex].src;
+    modalText.textContent = images[currentImageIndex].text;
+}
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = "none";
+}
+
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    const modalImg = document.getElementById('modalImg');
+    const modalText = document.getElementById('modalText');
+    modalImg.src = images[currentImageIndex].src;
+    modalText.textContent = images[currentImageIndex].text;
+}
